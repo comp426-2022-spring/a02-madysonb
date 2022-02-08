23bc8072fac03cdd792b2ce4258808b2c8f0cc50
Madyson Barber <madysonb@live.unc.edu>
2022-02-07 21:17:15 -0500

    call input and check working

--- guess-flip.js
+++ guess-flip.js
@@ -1,6 +1,13 @@
 // Import coin functions
 import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs"
 import minimist from 'minimist'
-// Take one argument on the command line named --call that will only accept .
+// Take one argument on the command line named --call
+const args = minimist(process.argv.slice(2))
+args['call']
 // If no argument is given, or something other than “heads” or “tails” is passed with --call, return an error message (see below for example).
+let guess = args.call
+if (guess !== 'tails' & guess !== 'heads') {
+    console.log('Error: no input.\nUsage: node guess-flip --call=[heads|tails]')
+    process.exit(1)
+}
 // Compare the call with a flip and report the result to STDOUT (see below)
