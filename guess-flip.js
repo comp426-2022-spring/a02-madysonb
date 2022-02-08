44c000161a2346767892d82ef4ea92d1e359b5bd
Madyson Barber <madysonb@live.unc.edu>
2022-02-07 21:18:17 -0500

    guess-flip.js working

--- guess-flip.js
+++ guess-flip.js
@@ -10,4 +10,5 @@ if (guess !== 'tails' & guess !== 'heads') {
     console.log('Error: no input.\nUsage: node guess-flip --call=[heads|tails]')
     process.exit(1)
 }
-// Compare the call with a flip and report the result to STDOUT (see below)
+// Compare the call with a flip and report the result to STDOUT
+console.log(flipACoin(guess))
