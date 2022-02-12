e0b46515f7ca7d495a686cf2e06288b5a7b07fb9
Madyson Barber <madysonb@live.unc.edu>
2022-02-12 12:45:30 -0500

    trying splitting error message into 2 logs

--- guess-flip.js
+++ guess-flip.js
@@ -6,8 +6,9 @@ const args = minimist(process.argv.slice(2))
 args['call']
 // If no argument is given, or something other than “heads” or “tails” is passed with --call, return an error message (see below for example).
 let guess = args.call
-if (guess !== 'tails' & guess !== 'heads' & guess !== null) {
-    console.log('Error: no input.\nUsage: node guess-flip --call=[heads|tails]')
+if (guess !== 'tails' & guess !== 'heads') {
+    console.log('Error: no input.')
+    console.log('Usage: node guess-flip --call=[heads|tails]')
     process.exit(1)
 }
 // Compare the call with a flip and report the result to STDOUT
